aefa28647c0a514b16fbd39881121bf98ec81299
Aryaan Hegde <aryhegde@gmail.com>
2022-05-21 21:28:16 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -36,13 +36,23 @@ function App() {
         setCopyState(true);
     }
 
+    const passwordRating = {
+        width: statePassword !== "" ? (`${(70 / 4) * (passwordStrength(statePassword).id + 1)}vw`) : "0",
+        background: statePassword !== "" ? ((passwordStrength(statePassword).id === 0 || passwordStrength(statePassword).id === 1) ? "#D0312D" : (passwordStrength(statePassword).id === (2) ? "#FFBA01" : "#3d8c40")) : "rgb(192, 192, 192)"
+    }
+
     return (
         <div className="container my-4">
             <h1 className="password-heading">Password generator</h1>
 
             <div className="password-holder">
                 <input type="text" onChange={(e) => setStatePassword(e.target.value)} value={statePassword} className="password-input" />
+                <div className="password-rating">
+                    <div style={passwordRating} className="inner-password-rating"></div>
+                </div>
+
                 <button onClick={copyPassword} className="inline-btn btn btn-primary">{copyState ? "Copied" : "Copy"}</button>
+                
                 <div className="configurations">
                     <input onChange={(e) => setUppercase(e.target.checked)} type="checkbox" id="uppercase" className="label" checked={uppercase} />
                     <label htmlFor="uppercase">Uppercase</label>
@@ -53,11 +63,13 @@ function App() {
                     <input onChange={(e) => setSymbol(e.target.checked)} type="checkbox" id="symbol" className="label" checked={symbol} />
                     <label htmlFor="symbol">Symbols</label>
                 </div>
+
                 <div className="password-length">
                     <label htmlFor="range" className="block-label">Password length</label>
-                    <input onChange={(e) => setRangeValue(e.target.value)} type="number" className="password-length-input" value={rangeValue} />
+                    <input onChange={(e) => e.target.value >= 1 && e.target.value <= 50 && setRangeValue(e.target.value)} type="number" className="password-length-input" value={rangeValue} />
                     <input type="range" onChange={(e) => setRangeValue(e.target.value)} id="range" min="1" value={rangeValue} max="50" />
                 </div>
+
                 <button className="btn btn-primary" onClick={getPassword}>Get password</button>
             </div>
         </div>
